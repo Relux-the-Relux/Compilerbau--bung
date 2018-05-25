eb3a6045181bad83b31af0eab94f49a5e836d6e6
PhexSerath <34057569+PhexSerath@users.noreply.github.com>
2018-05-25 12:26:05 +0200

    Add files via upload

--- uebung2/c1-scanner/testscanner.c
+++ uebung2/c1-scanner/testscanner.c
@@ -8,9 +8,6 @@ yystype yylval;
 int main(int argc, char* argv[])
 {
 
-	FILE *datei;
-	datei = fopen("ausgabe.txt","w");
-
 	int token;
 
 	if (argc != 2)
@@ -27,53 +24,35 @@ int main(int argc, char* argv[])
 
 	while ((token = yylex()))
 	{
-		fprintf(datei,"Line: %3d\t", yylineno);
+		printf("Line: %3d\t", yylineno);
 		switch (token)
 		{
 		case ID:
-			fprintf(datei,"ID:     %s\n", yylval.string);
+			printf("ID:     %s\n", yylval.string);
 			break;
 			
 		case CONST_BOOLEAN:
-			fprintf(datei,"BOOL:   %s\n", yylval.intValue ? "true" : "false");
+			printf("BOOL:   %s\n", yylval.intValue ? "true" : "false");
 			break;
 			
 		case CONST_INT:
-			fprintf(datei,"INT:    %d\n", yylval.intValue);
+			printf("INT:    %d\n", yylval.intValue);
 			break;
 			
 		case CONST_FLOAT:
-			fprintf(datei,"FLOAT:  %g\n", yylval.floatValue);
+			printf("FLOAT:  %g\n", yylval.floatValue);
 			break;
 			
 		case CONST_STRING:
-			fprintf(datei,"STRING: %s\n", yylval.string);
+			printf("STRING: %s\n", yylval.string);
 			break;
 			
 		default:
 			if (token <= 255)
-				fprintf(datei,"Token: '%c'\n", token);
+				printf("Token: '%c'\n", token);
 			else
-				fprintf(datei,"Token: %d\n", token);
+				printf("Token: %d\n", token);
 		}
 	}
-	fclose(datei);
-	datei = fopen("ausgabe.txt","r");
-	FILE *datei2;
-	datei2 = fopen("demorgan.sol","r");
-	char temp1[260], temp2[260];
-	int i =0;
-	while(fgets(temp1, 50, datei)){
-		fgets(temp2, 50, datei2);
-		//printf("t1: %s\n",temp1);
-		//printf("t2: %s\n",temp2);
-		i++;
-		if(strcmp(temp1,temp2)!=0){
-			printf("error in line %d\n",i);
-			exit(-1);
-		}
-	}
-	fclose(datei);
-	fclose(datei2);
 	return 0;
 }
